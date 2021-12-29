3799160cd3a856cfac8f8df9a7dc056358958960
Deep Patel <deeppatel1944@gmail.com>
2021-12-29 17:00:09 -0500

    adding game function

--- rsp.js
+++ rsp.js
@@ -1,6 +1,39 @@
 function computerPlay(){
-    const plays = ['rock','paper','scissors']
+    const plays = ['ROCK','PAPER','SCISSORS']
     return plays[Math.floor(Math.random() * 3)];
 }
 
-console.log(computerPlay())
+function playRound(playerSelection,computerSelection){
+    playerSelection=playerSelection.toUpperCase()
+    if (playerSelection=='ROCK' && computerSelection=='PAPER'){
+        return `You lose! Rock beats Paper`
+    }
+        else if (playerSelection=='PAPER' && computerSelection=='ROCK'){
+            return `You win! Rock beats Paper`
+        }
+        else if (playerSelection=='PAPER' && computerSelection=='SCISSORS'){
+            return `You lose! Scissors beats Paper`
+        }
+        else if (playerSelection=='SCISSORS' && computerSelection=='PAPER'){
+            return `You win! Scissors beats Paper`
+        }
+        else if (playerSelection=='ROCK' && computerSelection=='SCISSORS'){
+            return `You win! Rock beats Scissor`
+        }
+        else if (playerSelection=='SCISSORS' && computerSelection=='ROCK'){
+            return `You lose! Rock beats Scissor`
+        }
+        else if (playerSelection=='PAPER' && computerSelection=='PAPER'){
+            return `You tie!`
+        }
+        else if (playerSelection=='ROCK' && computerSelection=='ROCK'){
+            return `You tie!`
+        }
+        else if (playerSelection=='SCISSORS' && computerSelection=='SCISSORS'){
+            return `You tie!`
+        }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
